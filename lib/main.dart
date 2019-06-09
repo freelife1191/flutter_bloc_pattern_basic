0087f4a036a2ed29a3e4fa00d64784b1cac6d113
freelife1191 <freelife1191.good@gmail.com>
2019-06-09 23:44:39 +0900

    카운터 앱 - RxDart, BehaviorSubject StreamBuilder를 사용해 Reactive 프로그래밍 방식으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:rxdart/rxdart.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,8 +26,8 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
+  final counterSubject = BehaviorSubject<int>(); //마지막 값을 가지고 있고 얻게 해줌
   int counter = 0;
-  
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,15 +36,21 @@ class _CounterState extends State<Counter> {
         children: <Widget>[
           RaisedButton(
             child: Text('add'),
-            onPressed: () {// 버튼 클릭시 카운터 증가
-              setState(() {
-                counter++;
-              });
+            onPressed: () {// 버튼 클릭시 카운터 증가하고 counterSubject에 넣어 줌
+              counterSubject.add(++counter);
             },
           ),
-          Text(
-            '$counter',
-            style: TextStyle(fontSize: 30),
+          StreamBuilder<int>(
+            stream: counterSubject.stream, //counterSubject에 들어간 값들이 연결이 됨
+              initialData: 0, // 아무 값이 들어있지 않으므로 초기값 지정
+            builder: (context, snapshot) {
+              if (snapshot.hasData){
+                return Text(
+                  '${snapshot.data}',
+                  style: TextStyle(fontSize: 30),
+                );
+              }
+            }
           )
         ],
       ),
